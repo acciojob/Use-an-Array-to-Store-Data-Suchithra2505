97a8ed9bd2d17584fb4b364bf58e837770965b17
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-05-28 16:16:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let yourArray = [
+  "hello",  // String
+  42,       // Number
+  true,     // Boolean
+  { key: "value" }, // Object
+  [1, 2, 3] // Array
+];
+
+console.log(yourArray.length); // logs 5
+console.log(yourArray);
 
